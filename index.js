230bc4230da93fe92412b068dc111b0446dbc989
Flying Musk <musk.flying@gmail.com>
2023-07-23 16:55:26 +0800

    Make gcloud init, add the secon end point, not yet deploy to GCP.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const functions = require('@google-cloud/functions-framework');
 functions.http('login', (req, res) => {
   res.send('hello, please login');
 });
+
+functions.http('getData', (req, res) => {
+  res.send('getData success');
+});
