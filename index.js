76f5eca1eb5267ce7689a5eb1f8344ab52aec3d1
Flying Musk <musk.flying@gmail.com>
2023-07-23 20:39:09 +0800

    Change to use POST for login function.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('login', (req, res) => {
-  res.send('hello, please login');
+  if (req.method === 'POST') {
+    const { username, password } = req.body;
+    console.log(req.body, username, password);
+    res.status(200).json({ success: true, message: 'Login successful' });
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
 });
 
 functions.http('getData', (req, res) => {
